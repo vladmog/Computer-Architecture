8b83d996a7ea8dd2a1410d59795305b53bbacab2
vladmog <vladmog@gmail.com>
2019-12-20 02:54:53 -0800

    Day 4 MVP complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,6 +9,9 @@ PRN = 0b01000111
 MUL = 0b10100010
 PUSH = 0b01000101
 POP = 0b01000110
+CALL  = 0b01010000 # 00000rrr
+RET = 0b00010001
+ADD = 0b10100000 # 00000aaa 00000bbb
 
 class CPU:
     """Main CPU class."""
@@ -19,14 +22,19 @@ class CPU:
         self.fl = 0
         self.ram = [0] * 256 # 256 bytes
         self.reg = [0] * 9
+
         self.reg[7] = self.ram[0xF4]
         self.sp = self.reg[7]
+        
         self.branchtable = {}
         self.branchtable[LDI] = self.handle_ldi
         self.branchtable[PRN] = self.handle_prn
         self.branchtable[MUL] = self.handle_mul
         self.branchtable[PUSH] = self.handle_push
         self.branchtable[POP] = self.handle_pop
+        self.branchtable[CALL] = self.handle_call
+        self.branchtable[RET] = self.handle_ret
+        self.branchtable[ADD] = self.handle_add
         pass
 
     def handle_ldi(self):
@@ -46,6 +54,12 @@ class CPU:
         register2 = self.ram[self.pc + 2]
         self.alu("MUL", register1, register2)
         self.pc += 3
+
+    def handle_add(self):
+        register1 = self.ram[self.pc + 1]
+        register2 = self.ram[self.pc + 2]
+        self.alu("ADD", register1, register2)
+        self.pc +=3
     
     def handle_push(self):
         self.sp -= 1
@@ -60,6 +74,24 @@ class CPU:
         self.reg[register] = self.ram[self.sp]
         self.sp += 1
         self.pc += 2
+
+    def handle_call(self):
+        print("CALL")
+        # Push next instruction location to stack
+        self.sp -= 1
+        return_address = self.pc + 2
+        self.ram[self.sp] = return_address
+        print(f"Saving return address of {return_address} to stack")
+
+        register = self.ram[self.pc + 1]
+        print(f"Jumping from {self.pc} to {self.reg[register]}")
+        self.pc = self.reg[register]
+
+    def handle_ret(self):
+        print("RET")
+        # Pop the value from the top of the stack and store it in the PC.
+        self.pc = self.ram[self.sp]
+        self.sp += 1
 
     def ram_read(self, pc):
         print(self.ram[pc])
@@ -109,6 +141,8 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
+            print(f"REG = {self.reg}")
+            print(f"Adding reg[{reg_a}] which is {self.reg[reg_a]} with reg[{reg_b}] which is {self.reg[reg_b]}")
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             print(f"REG = {self.reg}")
@@ -142,6 +176,7 @@ class CPU:
         running = True
         
         while running:
+            print(f"\nLine {self.pc + 1}: {bin(self.ram[self.pc])}")
             if self.ram[self.pc] == HLT:
                 running = False
             else:
